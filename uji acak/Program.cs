62cd7f814ece3487863020e5e65272616d9cabaa
Raden Darajat Bambang Eka Gunawan <send.raden@gmail.com>
2022-01-11 16:37:06 +0800

    Add project files.

---
+++ uji acak/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace uji_acak
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            Random ac1 = new Random();
+            int val1 = ac1.Next(-5,5);
+
+            Console.WriteLine(val1);
+        }
+    }
+}
