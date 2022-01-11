fdde159744c7d1dc377fe0dc7478feb20c0603f5
Raden Darajat Bambang Eka Gunawan <send.raden@gmail.com>
2022-01-11 16:39:38 +0800

    random double

--- uji acak/Program.cs
+++ uji acak/Program.cs
@@ -7,10 +7,14 @@ namespace uji_acak
         static void Main(string[] args)
         {
             //Console.WriteLine("Hello World!");
+            
             Random ac1 = new Random();
-            int val1 = ac1.Next(-5,5);
+            //int val1 = ac1.Next(-5,5);
 
-            Console.WriteLine(val1);
+            //Console.WriteLine(val1);
+            double b1 = ac1.NextDouble();
+           // Console.WriteLine(b1);// <--- number from 0 to 1.0
+            Console.WriteLine(b1*100);
         }
     }
 }
