915328dff15b7b53a51d68b6c591bdfb731e9c6e
Raden Darajat Bambang Eka Gunawan <send.raden@gmail.com>
2022-01-11 16:44:52 +0800

    add coinflip 5x

--- uji acak/Program.cs
+++ uji acak/Program.cs
@@ -11,13 +11,25 @@ namespace uji_acak
             Random ac1 = new Random();
             //int val1 = ac1.Next(-5,5);
 
-            //Console.WriteLine(val1);
-            double b1 = ac1.NextDouble();
-           // Console.WriteLine(b1);// <--- number from 0 to 1.0
-            Console.WriteLine(b1*100);
-            Console.WriteLine((float)b1 * 100F);
-            // Console.WriteLine((float)b1 * 100M); /// <---WRONG 
-            Console.WriteLine((decimal)b1 * 100M);
+            // //Console.WriteLine(val1);
+            // double b1 = ac1.NextDouble();
+            //// Console.WriteLine(b1);// <--- number from 0 to 1.0
+            // Console.WriteLine(b1*100);
+            // Console.WriteLine((float)b1 * 100F);
+            // // Console.WriteLine((float)b1 * 100M); /// <---WRONG 
+            // Console.WriteLine((decimal)b1 * 100M);
+
+            
+            
+            //coinflip 5x:
+
+            for (int i = 0; i < 6; i++)
+            {
+                int zeroOrOne = ac1.Next(2);
+                bool coinFlip = Convert.ToBoolean(zeroOrOne);
+                Console.WriteLine(coinFlip);
+            }
+           
 
 
         }
